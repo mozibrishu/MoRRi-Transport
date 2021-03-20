78e4f065bdf3810ee1ba850751b7d59b9133853f
mozibrishu <mozib.diu@gmail.com>
2021-03-20 08:35:01 +0600

    form hook checking

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { useContext } from 'react';
 import { UserContext } from '../../App';
 import { useHistory, useLocation } from 'react-router-dom';
-import { initializeLoginFramework, handleGoogleSignIn, handleSignOut, signInWithEmailAndPassword,createUserWithEmailAndPassword } from './LoginManager';
+import { initializeLoginFramework, handleGoogleSignIn, handleSignOut, signInWithEmailAndPassword, createUserWithEmailAndPassword } from './LoginManager';
+import './Login.css'
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
     const [newUser, setNewUser] = useState(false);
@@ -13,6 +15,7 @@ const Login = () => {
         password: '',
         photo: ''
     });
+    
 
     initializeLoginFramework();
 
@@ -24,62 +27,77 @@ const Login = () => {
 
     const googleSignIn = () => {
         handleGoogleSignIn()
-        .then(res => {
-          handleResponse(res, true);
-        })
+            .then(res => {
+                handleResponse(res, true);
+            })
     }
-  
+
     const signOut = () => {
         handleSignOut()
-        .then(res => {
-            handleResponse(res, false);
-        })
+            .then(res => {
+                handleResponse(res, false);
+            })
     }
-  
-    const handleResponse = (res, redirect) =>{
-      setUser(res);
-      setLoggedInUser(res);
-      if(redirect){
-          history.replace(from);
-      }
+
+    const handleResponse = (res, redirect) => {
+        setUser(res);
+        setLoggedInUser(res);
+        if (redirect) {
+            history.replace(from);
+        }
     }
 
     const handleBlur = (e) => {
         let isFieldValid = true;
-        if(e.target.name === 'email'){
-          isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+        if (e.target.name === 'email') {
+            isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
         }
-        if(e.target.name === 'password'){
-          const isPasswordValid = e.target.value.length > 6;
-          const passwordHasNumber =  /\d{1}/.test(e.target.value);
-          isFieldValid = isPasswordValid && passwordHasNumber;
+        if (e.target.name === 'password') {
+            const isPasswordValid = e.target.value.length > 6;
+            const passwordHasNumber = /\d{1}/.test(e.target.value);
+            isFieldValid = isPasswordValid && passwordHasNumber;
         }
-        if(isFieldValid){
-          const newUserInfo = {...user};
-          newUserInfo[e.target.name] = e.target.value;
-          setUser(newUserInfo);
+        if (isFieldValid) {
+            const newUserInfo = { ...user };
+            newUserInfo[e.target.name] = e.target.value;
+            setUser(newUserInfo);
         }
-      }
-      const handleSubmit = (e) => {
-        if(newUser && user.email && user.password){
-          createUserWithEmailAndPassword(user.name, user.email, user.password)
-          .then(res => {
-            handleResponse(res, true);
-          })
+    }
+    const handleSubmit = (e) => {
+        if (newUser && user.email && user.password) {
+            createUserWithEmailAndPassword(user.name, user.email, user.password)
+                .then(res => {
+                    handleResponse(res, true);
+                })
         }
-    
-        if(!newUser && user.email && user.password){
-          signInWithEmailAndPassword(user.email, user.password)
-          .then(res => {
-            handleResponse(res, true);
-          })
+
+        if (!newUser && user.email && user.password) {
+            signInWithEmailAndPassword(user.email, user.password)
+                .then(res => {
+                    handleResponse(res, true);
+                })
         }
         e.preventDefault();
-      }
+    }
 
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="d-flex container-fluid row align-items-center">
+            <div className="login col-11 col-md-9 col-lg-7 text-center">
+                <h1>Our own Authentication</h1>
+                <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id="" />
+                <label htmlFor="newUser">New User Sign up</label>
+                <form onSubmit={handleSubmit}>
+                    {newUser && <input name="name" type="text" onBlur={handleBlur} placeholder="Your name" />}
+                    <br />
+                    <input type="text" name="email" onBlur={handleBlur} placeholder="Your Email address" required />
+                    <br />
+                    <input type="password" name="password" onBlur={handleBlur} placeholder="Your Password" required />
+                    <br />
+                    <input type="submit" value={newUser ? 'Sign up' : 'Sign in'} />
+                </form>
+                <p style={{ color: 'red' }}>{user.error}</p>
+                {user.success && <p style={{ color: 'green' }}>User {newUser ? 'created' : 'Logged In'} successfully</p>}
+            </div>
         </div>
     );
 };
