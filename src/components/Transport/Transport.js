028c848d25603e36892e561e5f23a1d0518e5032
mozibrishu <mozib.diu@gmail.com>
2021-03-20 07:46:51 +0600

    Login part started

--- src/components/Transport/Transport.js
+++ src/components/Transport/Transport.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Transport.css'
 
 const Transport = (props) => {
-    const {transportType,imageURL} = props.transport;
+    const { id,transportType, imageURL } = props.transport;
     return (
         <div className=" col-12 col-md-6 col-lg-3 text-center transport-card-container">
-            <div className="transport-card">
-                <img src={imageURL} alt="" srcset=""/>
-                <h2>{transportType}</h2>
-            </div>
-            
+            <Link to={`/destination/${id}`}>
+                <div className="transport-card">
+                    <img src={imageURL} alt="" srcset="" />
+                    <h2>{transportType}</h2>
+                </div>
+            </Link>
+
         </div>
     );
 };
